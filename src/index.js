1ab1e4757e7137ea3b8ec7a9832b961847807338
Valeryaleshka <valeryaleshka@gmail.com>
2020-10-01 13:45:33 +0300

    feat: task done

--- src/index.js
+++ src/index.js
@@ -1,12 +1,40 @@
 
 exports.min = function min (array) {
-  return 0;
+    if( !array || (array.length < 1)){
+        return 0;
+    }
+    var min = array[0];
+    for( var i = 0; i < array.length; i++ ){
+        if(array[i]<min){
+            min = array[i];
+        }
+    }
+    return min;
 }
 
 exports.max = function max (array) {
-  return 0;
+
+    if( !array || (array.length < 1)){
+        return 0;
+    }
+    var max = array[0];
+    for( var i = 0; i < array.length; i++ ){
+        if(array[i]>max){
+            max = array[i];
+        }
+    }
+    return max;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if( !array || (array.length < 1)){
+        return 0;
+    }
+    
+    var sum = 0;
+    for( var i = 0; i < array.length; i++ ){
+        sum += parseInt( array[i], 10 );
+    }
+    
+    return sum/array.length;
 }
